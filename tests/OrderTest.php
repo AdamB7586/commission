2779a7a63c1905dcf65684b9d9393cd97fd56985
Adam Binnersley <abinnersley@gmail.com>
2019-07-02 14:42:58 +0100

    Change requirements

--- tests/OrderTest.php
+++ tests/OrderTest.php
@@ -10,7 +10,7 @@ class OrderTest extends TestCase{
     protected $db;
     protected $commission;
     
-    protected function setUp() {
+    protected function setUp(): void {
         $this->db = new Database($GLOBALS['hostname'], $GLOBALS['username'], $GLOBALS['password'], $GLOBALS['database']);
         if(!$this->db->isConnected()){
             $this->markTestSkipped(
@@ -26,7 +26,7 @@ class OrderTest extends TestCase{
         $this->commission = new Commission($this->db, new Config($this->db, 'store_config'));
     }
     
-    protected function tearDown() {
+    protected function tearDown(): void {
         $this->db = null;
         $this->commission = null;
     }
